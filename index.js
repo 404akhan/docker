5442c375dd5e2557adb413902665e7712b26238c
Akhan Ismailov <404akhan@gmail.com>
2016-12-23 17:09:45 +0800

    default

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('From Docker. Adding Jenkins. Someword.')
+  res.send(' CHANGING NOW . From Docker. Adding Jenkins. Someword.')
 })
 
 app.listen(3000, function () {
