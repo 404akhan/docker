6a69d55113a4d5fb1a2d46e2defe45abb0a7eb66
Akhan Ismailov <404akhan@gmail.com>
2016-12-23 17:06:27 +0800

    default

--- index.js
+++ index.js
@@ -2,12 +2,10 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World! From Docker. Adding Jenkins.')
+  res.send('Hello World! From Docker. Adding Jenkins. Someword.')
 })
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
- 
- 
- 
+
